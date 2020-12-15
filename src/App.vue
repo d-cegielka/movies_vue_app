ecee587c1b6304852c84811e5ef3edb921b58796
kamilZ01 <224546@edu.p.lodz.pl>
2020-12-15 23:47:51 +0100

    added genres.json file, created movies list ordered by genres from genres.json

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
     <h1>Baza filmów</h1>
     <SearchEngine/>
     <MovieTable/>
+    <MovieListByGenre/>
   </b-container>
 </template>
 
 <script>
 import SearchEngine from './components/SearchEngine.vue'
 import MovieTable from "@/components/MovieTable";
+import MovieListByGenre from "@/components/MovieListByGenre";
 
 export default {
   name: 'App',
   components: {
+    MovieListByGenre,
     SearchEngine,
     MovieTable
   }
---
+++ src/components/MovieListByGenre.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Filmy według gatunku</h1>
+    <ol v-for="item in items" :key="item">
+        <h4 v-if="getMoviesByGenre(item).length !== 0">{{item}}</h4>
+        <li v-for="(movie,index) in getMoviesByGenre(item) " :key="index" >
+          {{movie.title}}
+        </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+import genres from '../assets/movies/genres.json'
+import movies from '../assets/movies/movies.json'
+import {_} from 'vue-underscore';
+
+export default {
+name: "MovieListByGenre",
+
+  data(){
+
+    return {
+    items: genres
+  };
+  },
+ methods:{
+  getMoviesByGenre: function (genre) {
+    return _.filter(movies,function (movie) {
+     return _.contains(movie.genres,genre)
+    });
+  }
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
